36d7840b33e0588fbf78f3b706bcc4d0a8d75105
Uday Solanki <Udaysolanki530@gmail.com>
2024-09-13 11:29:28 +0530

    hel

--- src/Components/About/Team.jsx
+++ src/Components/About/Team.jsx
@@ -1,32 +1,69 @@
 import React from "react";
+import { Data } from "./Teamdata";
+import { MDBIcon } from "mdb-react-ui-kit";
 
 function Team() {
   return (
-    <div className="container-fluid d-flex justify-content-center align-items-center min-vh-100">
-      <div className="text-center py-5 px-5">
-        <div className="text-primary fw-light display-5">
-          Team page is work in progess..
-          <span>
-            <i className="fa fa-spinner spinning" aria-hidden="true"></i>
+    <div className="">
+      <header className="bg-white text-white text-center  py-5 position-relative">
+        {/* Navigation Path */}
+        <div
+          className="position-absolute top-0 start-0 ms-0 mt-0 p-4 fs-5 fw-bold"
+          style={{ zIndex: 100 }}
+        >
+          <span className="small text-primary">
+            <a href="#" className="">
+              / Meet Leadership
+            </a>{" "}
+            {/* <a href="#">Education</a> */}
           </span>
         </div>
-      </div>
-      <style jsx>{`
-        .spinning {
-          font-size: 3rem; /* Adjust size if needed */
-          display: inline-block;
-          animation: spin 2s linear infinite;
-        }
 
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
+        {/* Header Content */}
+        <div className="container text-primary mt-5">
+          <h1 className="display-6">
+            Visionary Leadership: Driving Innovation and Impact
+          </h1>
+          <p className="lead">
+            Uniting strategic insights and forward-thinking solutions to guide
+            our company toward excellence. Meet the leaders shaping our future
+            with transformative vision and expertise.
+          </p>
+        </div>
+      </header>
+
+      <section class="">
+        <div class="container px-5 ">
+          <section class="py-5 ">
+            <div class="container px-5 my-5">
+              <div class="text-center">
+                <h2 class="fw-light text-danger mb-5 half-underline">
+                  Our Visionaries and Trailblazers
+                </h2>
+                {/* <p class="lead fw-normal   text-primary mb-5">
+                Guiding with insight, expertise, and a relentless drive for innovation
+                </p> */}
+              </div>
+              <div class="row gx-5 row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center">
+                {Data.map((team, index) => (
+                  <div class="col mb-5   mt-5 mb-xl-0">
+                    <div class="text-center bg-light rounded-3 p-4" key={index}>
+                      <img
+                        class="img-fluid rounded-circle mb-2 px-4"
+                        src={team.img}
+                        alt="..."
+                      />
+                      <h5 class="fw-bolder text-primary">{team.Name} <span ><a href={team.linkedin} className=""> <MDBIcon color='primary' className="fs-4" fab icon='linkedin' /></a></span></h5> 
+                      <div class=" text-danger">{team.Desc}</div>
+                      
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/Components/About/Teamdata.js
@@ -0,0 +1,50 @@
+export const Data = [
+    {
+        Name:'vikram chouhan',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'Bhargav xyz ',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'rajnish jaiswal',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'renju karunkaran',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'milan xyz',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'Shyam xyz',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'Shivendra kadam',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+    {
+        Name:'John Doe',
+        Desc : 'Occupation',
+        linkedin :'www.linkedin.com/in/udaysolankii',
+        img : 'https://dummyimage.com/150x150/ced4da/6c757d',
+    },
+]
