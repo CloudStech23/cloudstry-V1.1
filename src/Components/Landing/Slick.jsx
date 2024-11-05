3cf44001bb84ab6fab049c99b1cb06b921bdc142
Uday Solanki <Udaysolanki530@gmail.com>
2024-11-05 12:32:31 +0530

    new update

--- src/Components/Landing/Slick.jsx
+++ src/Components/Landing/Slick.jsx
@@ -148,7 +148,7 @@ export default function SwiperCarousel() {
               </div>
 
               <MDBBtn
-                href="https://agent511.com/contact-us/"
+                href=""
                 target="_blank"
                 color="danger"
                 className="px-4  py-2 mt-4"
